b065076db0ffb1d98a5fba91c42e207ccf9c36ef
Anshu Sinha <anshujuly2@gmail.com>
2023-03-02 10:23:18 +0530

    Add default parameters codes

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// Default parameters
+
+// const bookings = [];
+
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // ES5 version
+//   //   numPassengers = numPassengers || 1;
+//   //   price = price || 199;
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking("LH123");
+// createBooking("LH124", 4);
+// createBooking("LH124", undefined, 1000);
+
+/////
