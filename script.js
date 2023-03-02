e38f7119b21cf3cc335bd6015f3ca13a61dfd6f8
Anshu Sinha <anshujuly2@gmail.com>
2023-03-02 10:41:33 +0530

    Add value vs reference in functions codes

--- script.js
+++ script.js
@@ -26,3 +26,34 @@
 // createBooking("LH124", undefined, 1000);
 
 /////
+
+// 129. How passing arguments works: value vs reference
+
+// const flight = "LH123";
+// const anshu = {
+//   name: "Anshu Sinha",
+//   passportNum: 53534535234,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = "LH2334";
+//   passenger.name = `Mr. ${passenger.name}`;
+
+//   if (passenger.passportNum === 5353453523) {
+//     console.log("Checked in!");
+//   } else {
+//     console.log("Wrong passport!");
+//   }
+// };
+
+// checkIn(flight, anshu);
+// console.log(flight);
+// console.log(anshu);
+
+// const newPassport = function (person) {
+//   person.passportNum = Math.trunc(Math.random() * 1000000000);
+// };
+
+// newPassport(anshu);
+// checkIn(flight, anshu);
+// console.log(anshu);
