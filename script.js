72c64892a694a85e45cb924902ceedeac0f78684
Anshu Sinha <anshujuly2@gmail.com>
2023-03-03 10:25:48 +0530

    Add coding challenge 1 codes

--- script.js
+++ script.js
@@ -186,3 +186,59 @@
 // document
 //   .querySelector(".buy")
 //   .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+
+/////
+
+// Coding challenge #1
+
+// Let's build a simple poll app!
+// A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter 'poll' object below.
+// Your tasks:
+// 1. Createamethodcalled'registerNewAnswer'onthe'poll'object.The method does 2 things:
+// 1.1. Display a prompt window for the user to input the number of the
+// selected option. The prompt should look like this: What is your favourite programming language?
+// 0: JavaScript
+// 1: Python
+// 2: Rust
+// 3: C++
+// (Write option number)
+// 1.2. Based on the input number, update the 'answers' array property. For example, if the option is 3, increase the value at position 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g. answer 52 wouldn't make sense, right?)
+// 2. Callthismethodwhenevertheuserclicksthe"Answerpoll"button.
+// 3. Createamethod'displayResults'whichdisplaysthepollresults.The
+// method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1".
+// 4. Runthe'displayResults'methodattheendofeach 'registerNewAnswer' method call.
+
+// const poll = {
+//   question: "What is your favourite programming language?",
+//   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+//   // This generates [0, 0, 0, 0]. More in the next section! answers: new Array(4).fill(0),
+//   answers: new Array(4).fill(0),
+
+//   registerNewAnswer: function () {
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+//       )
+//     );
+//     if (answer >= 0 && answer < this.options.length) {
+//       this.answers[answer] += 1;
+//     } else {
+//       alert(`Answer ${answer} wouldn't make sense right!`);
+//     }
+
+//     const type = prompt("Enter the result type, 'array' or 'string'");
+//     this.displayResults(type);
+//   },
+
+//   displayResults: function (type = "array") {
+//     if (type == "array") {
+//       alert(this.answers);
+//     } else if (type == "string") {
+//       alert(`Poll results are ${this.answers.join(", ")}`);
+//     }
+//   },
+// };
+
+// document
+//   .querySelector(".poll")
+//   .addEventListener("click", poll.registerNewAnswer.bind(poll));
